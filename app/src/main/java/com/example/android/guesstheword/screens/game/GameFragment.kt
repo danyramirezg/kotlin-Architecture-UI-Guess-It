a556b984637f6c47de33fac8a50e6c29f2cc71e5
danyramirezg <danyramirezg@gmail.com>
2021-06-09 16:12:32 -0500

    Encapsulated LiveData and create an event

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -21,6 +21,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -73,6 +74,13 @@ class GameFragment : Fragment() {
             binding.wordText.text = newWord.toString()
         })
 
+        viewModel.eventGameFinish.observe(viewLifecycleOwner, Observer { hasFinished ->
+            if (hasFinished){
+                gameFinished()
+                viewModel.onGameFinishComplete()
+            }
+        })
+
         return binding.root
 
     }
@@ -86,7 +94,7 @@ class GameFragment : Fragment() {
         val currentScore = viewModel.score.value ?: 0
         val action = GameFragmentDirections.actionGameToScore(currentScore)
         findNavController(this).navigate(action)
-
+//        Toast.makeText(this.activity, "Game has finished!", Toast.LENGTH_SHORT).show()
     }
 
 }
--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,16 +1,28 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
-    // The current word
-    val word = MutableLiveData<String>()
-
+    // Internal variable:
     // The current score
-    val score = MutableLiveData<Int>()
+    private val _score = MutableLiveData<Int>()
+    // External
+    val score: LiveData<Int>
+        get() = _score
+
+    // The current word
+    private val _word = MutableLiveData<String>()
+    val word: LiveData<String>
+        get() = _word
+
+    private val _eventGameFinish = MutableLiveData<Boolean>()
+    val eventGameFinish: LiveData<Boolean>
+        get() = _eventGameFinish
+
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
@@ -19,9 +31,10 @@ class GameViewModel : ViewModel() {
 
     init {
         Log.i("Game view model", "GameViewModel created!")
+        _eventGameFinish.value = false
         resetList()
         nextWord()
-        score.value = 0
+        _score.value = 0
     }
 
     /**
@@ -60,9 +73,9 @@ class GameViewModel : ViewModel() {
     private fun nextWord() {
         //Select and remove a word from the list
         if (wordList.isEmpty()) {
-            //gameFinished()
+            _eventGameFinish.value = true
         } else {
-            word.value = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
     }
 
@@ -70,12 +83,12 @@ class GameViewModel : ViewModel() {
 
 
     fun onSkip() {
-        score.value = (score.value)?.minus(1)
+        _score.value = (score.value)?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score.value = (score.value)?.plus(1)
+        _score.value = (score.value)?.plus(1)
         nextWord()
     }
 
@@ -83,5 +96,9 @@ class GameViewModel : ViewModel() {
         super.onCleared()
         Log.i("Game view model", "GameViewModel destroyed!")
 
+    }
+
+    fun onGameFinishComplete(){
+        _eventGameFinish.value = false
     }
 }
